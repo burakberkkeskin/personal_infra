16e2d6bc6bd0e86d5434e95646bea55d131ed1ca
berk <burakberkkeskin@gmail.com>
2023-02-20 09:50:16 +0300

    variable moved to cloud

--- main/main.tf
+++ main/main.tf
@@ -16,7 +16,7 @@ module "ec2_instance" {
   instance_type = var.ec2_instance_type
   name = var.project_name
   key_name = var.key_name
-  public_key_path = var.ec2_public_key_path
+  public_key = var.ec2_public_key
   subnet_id = module.vpc.public_subnet_ids[0]
   security_group_ids = [module.vpc.egress_security_group_id ,module.vpc.ssh_security_group_id, module.vpc.http_security_group_id]
   user_data = var.ec2_user_data
@@ -26,25 +26,25 @@ module "ec2_instance" {
   ]
 }
 
-# module "load_balancer" {
-#   source = "../modules/application-load-balancer"
-#   name = var.project_name
-#   enable_deletion_protection = var.lb_enable_deletion_protection
-#   vpc_id = module.vpc.vpc_id
-#   security_groups = [module.vpc.http_security_group_id, module.vpc.egress_security_group_id]
-#   subnets = module.vpc.public_subnet_ids
-#   tags = var.lb_tags
-#   health_check_options = var.lb_health_check_options
-#   instance_ids = module.ec2_instance.instance_id
-#   listeners = var.lb_listeners
-# }
+module "load_balancer" {
+  source = "../modules/application-load-balancer"
+  name = var.project_name
+  enable_deletion_protection = var.lb_enable_deletion_protection
+  vpc_id = module.vpc.vpc_id
+  security_groups = [module.vpc.http_security_group_id, module.vpc.egress_security_group_id]
+  subnets = module.vpc.public_subnet_ids
+  tags = var.lb_tags
+  health_check_options = var.lb_health_check_options
+  instance_ids = module.ec2_instance.instance_id
+  listeners = var.lb_listeners
+}
 
-# module "cloudflare_dns" {
-#   source = "../modules/cloudflare_dns"
-#   api_token = var.cloudflare_api_token
-#   zone_id = var.cloudflare_zone_id
-#   record_name = var.cloudflare_record_name
-#   record_value = module.load_balancer.lb_dns_name
-#   record_type = var.cloudflare_record_type
-#   record_proxied = var.cloudflare_record_proxied
-# }
+module "cloudflare_dns" {
+  source = "../modules/cloudflare_dns"
+  api_token = var.cloudflare_api_token
+  zone_id = var.cloudflare_zone_id
+  record_name = var.cloudflare_record_name
+  record_value = module.load_balancer.lb_dns_name
+  record_type = var.cloudflare_record_type
+  record_proxied = var.cloudflare_record_proxied
+}
