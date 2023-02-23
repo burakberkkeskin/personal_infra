e2b09774079d3594469e187d0d11da26624fb1c8
berk <burakberkkeskin@gmail.com>
2023-02-23 12:12:57 +0300

    removed provider from module

--- main/provider.tf
+++ main/provider.tf
@@ -4,11 +4,19 @@ terraform {
       source  = "hashicorp/aws"
       version = "4.52"
     }
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+      version = "4.0.0"
+    }
   }
 }
 
 provider "aws" {
   region     = var.aws_region
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
 }
 
 terraform {
--- modules/cloudflare_dns/cloudflare_dns.tf
+++ modules/cloudflare_dns/cloudflare_dns.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    cloudflare = {
-      source = "cloudflare/cloudflare"
-      version = "4.0.0-rc2"
-    }
-  }
-}
-
-provider "cloudflare" {
-  api_token = var.api_token
-}
-
 resource "cloudflare_record" "cloudflare_dns" {
   zone_id = var.zone_id
   name    = var.record_name
