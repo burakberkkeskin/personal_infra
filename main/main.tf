3660063086ac02d53747f4a2f14ca9489608eb4a
burakberkkeskin <burakberkkeskin@gmail.com>
2024-02-04 10:36:23 +0300

    fixed all modules

--- main/main.tf
+++ main/main.tf
@@ -22,6 +22,74 @@ module "vpc" {
       cidr_block        = "10.10.41.0/24"
     }
   ]
+  ingress_security_groups = {
+    ssh = {
+      name        = "ssh"
+      description = "allow ssh ingress traffic"
+      ingress = [
+        {
+          description      = "Allow SSH traffic"
+          from_port        = "22"
+          to_port          = "22"
+          protocol         = "tcp"
+          cidr_blocks      = ["0.0.0.0/0"]
+          ipv6_cidr_blocks = ["::/0"]
+        }
+      ]
+    }
+    http = {
+      name        = "http"
+      description = "Allow HTTP(S) traffic"
+      ingress = [
+        {
+          description      = "Allow HTTP traffic"
+          from_port        = "80"
+          to_port          = "80"
+          protocol         = "tcp"
+          cidr_blocks      = ["0.0.0.0/0"]
+          ipv6_cidr_blocks = ["::/0"]
+        },
+        {
+          description      = "Allow HTTPS traffic"
+          from_port        = "443"
+          to_port          = "443"
+          protocol         = "tcp"
+          cidr_blocks      = ["0.0.0.0/0"]
+          ipv6_cidr_blocks = ["::/0"]
+        },
+      ]
+    }
+    wireguard = {
+      name        = "wireguard"
+      description = "Allow wireguard traffic"
+      ingress = [
+        {
+          description      = "Allow wireguard traffic"
+          from_port        = "51820"
+          to_port          = "51820"
+          protocol         = "udp"
+          cidr_blocks      = ["0.0.0.0/0"]
+          ipv6_cidr_blocks = ["::/0"]
+        }
+      ]
+    }
+  }
+  egress_security_groups = {
+    all = {
+      name        = "all"
+      description = "Allow All Egress traffic"
+      egress = [
+        {
+          description      = "Allow All Egress traffic"
+          from_port        = "0"
+          to_port          = "0"
+          protocol         = "-1"
+          cidr_blocks      = ["0.0.0.0/0"]
+          ipv6_cidr_blocks = ["::/0"]
+        },
+      ]
+    }
+  }
 }
 
 module "ec2_instance" {
@@ -35,7 +103,7 @@ module "ec2_instance" {
   key_name           = "fatheraws"
   public_key         = var.ec2_public_key
   subnet_id          = module.vpc.public_subnet_ids[0]
-  security_group_ids = [module.vpc.egress_security_group_id, module.vpc.ssh_security_group_id, module.vpc.http_security_group_id]
+  security_group_ids = [module.vpc.ingress_security_group_ids["ssh"], module.vpc.egress_security_group_ids["all"]]
   user_data          = var.ec2_user_data
   role_policy        = var.ec2_role_policy
   depends_on = [
@@ -48,7 +116,7 @@ module "load_balancer" {
   name                       = var.project_name
   enable_deletion_protection = false
   vpc_id                     = module.vpc.vpc_id
-  security_groups            = [module.vpc.http_security_group_id, module.vpc.egress_security_group_id]
+  security_groups            = [module.vpc.ingress_security_group_ids["http"], module.vpc.egress_security_group_ids["all"]]
   subnets                    = module.vpc.public_subnet_ids
   tags = {
     "Name" = "Main"
